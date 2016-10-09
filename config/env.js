ed7604093f3a56e36ffb7bd155a7c37284a94ca7
Axel Niklasson <axel.niklasson@live.com>
2016-10-09 18:14:21 +0200

    Removed travis

--- config/env.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-process.env.DB_DEV_URL = '';
-process.env.DB_TEST_URL = '';
-process.env.DB_PROD_URL = '';
-process.env.TOKEN_SECRET = '';
-process.env.GOOGLE_AUTH = '';
-
-fs.writeFileSync('./config/google-auth.json', process.env.GOOGLE_AUTH, 'utf-8');
