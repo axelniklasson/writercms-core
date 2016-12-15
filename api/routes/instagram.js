e4c73dce7d325d2f37087490a19e7d2aa2530fbb
Axel Niklasson <axel.niklasson@live.com>
2016-12-15 14:41:53 +0100

    Splice posts array in feed endpoint

--- api/routes/instagram.js
+++ api/routes/instagram.js
@@ -2,7 +2,7 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
-/* Get public instagram feed of user */
+/* Get public instagram feed of user (returns 9 latest posts) */
 router.get('/feed/:username', function(req, res) {
     var username = req.params.username;
     var reqString = 'https://www.instagram.com/' + username + '/media/';
@@ -10,7 +10,9 @@ router.get('/feed/:username', function(req, res) {
         if (error) {
             res.status(500).send('Could not get user feed. Error: ' + error);
         } else {
-            res.send(body);
+            var feed = JSON.parse(body);
+            feed.items.splice(9, 11);
+            res.json(feed.items);
         }
     });
 });
